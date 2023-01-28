1ba4b813ff01e391c061036cd3e2a1f30d09cf12
Santiago González Suárez <santiagogonzalezsuarez@gmail.com>
2023-01-28 02:58:22 +0100

    Actualizada lista de archivos

--- build/CMakeFiles/MarpajarosTPV.dir/MarpajarosTPV_autogen/EWIEGA46WW/qrc_Resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MarpajarosTPV.dir/MarpajarosTPV_autogen/EWIEGA46WW/qrc_Resources.cpp.o: \
- /home/santi/Proyectos/MarpajarosTPV/build/MarpajarosTPV_autogen/EWIEGA46WW/qrc_Resources.cpp \
- /usr/include/stdc-predef.h
